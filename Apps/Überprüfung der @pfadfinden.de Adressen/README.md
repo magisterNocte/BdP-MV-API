bfd877e142748c8a302b6420049107c099a51c13
magisterNocte <yannik.menge@gmail.com>
2022-01-31 16:16:19 +0100

    first commit

---
+++ Apps/Überprüfung der @pfadfinden.de Adressen/README.md
@@ -0,0 +1,27 @@
+Anwendung zur Überprüfung der @pfadfinden.de Email Adressen
+
+0. Einen MV Zugriff mit Rechten alle Accounts zu sehen in der userconf.py eintragen
+1. Liste mit allen Email Adressen von Outlook exportieren
+2. Datei in Excel als "outlook_data.xlsx" im "data" Ordner abspeichern 
+3. outlookconverter.p< ausführen
+4. Es wird erstellt...
+    - eine übersichtbare Tabelle aller Daten
+    - eine Tabelle mit allen Weiterleitungen
+    - eine Tabelle mit allen Postfächern
+    - eine Tabelle mit allen Adressen die nicht nach Personen benannt wurden
+5. App.bat ausführen 
+6. In den Tabellen werden ergänzt...
+    - die letzte Tätigkeit auf Bundesebene
+    - das Datum, wann sie beendet wurde
+7. Fehlermeldungen in der Tabelle
+    - kein Datum bedeutet, dass die Tätigkeit noch nicht beendet ist
+    - ERROR1 bedeutet, dass der user nicht in der MV ist
+    - "keine Bundestätigkeit" und das Datum "0000-00-00" bedeutet dass der user keine Bundestätigkeit in der MV stehen hat
+
+
+
+
+
+
+die Funktion looped durch alle user in einer Tabelle und ergänzt die letzte Tätigkeit auf Bundesebene und das Datum, wann sie beendet wurde
+        
---
+++ Apps/Überprüfung der @pfadfinden.de Adressen/outlookDataConverter.py
@@ -0,0 +1,38 @@
+"""
+Der code erstellt aus der Outlook user.csv 
+    1. eine übersichtbare Tabelle aller Daten
+    2. eine tabelle mit allen Weiterleitungen
+    3. eine Tabelle mit allen Postfächern
+    4. eine Tabelle mit allen Adressen die nicht nach Personen benannt wurden
+
+Dafür muss die user.csv als outlook_data.xlsx im data order des Projekts abgespeichert werden
+
+"""
+
+from openpyxl import Workbook, load_workbook
+
+from outlookDataConverterTools import excelHandling as exH
+
+# loadworkbook
+outlookSourceWb = load_workbook(exH.path + '\data\outlook_data.xlsx')
+outlookSourceWs = outlookSourceWb.active
+# new Workbooks
+outlookSortedWb = Workbook()
+outlookFilteredWbPostfach = Workbook()
+outlookFilteredWbWeiterleitung = Workbook()
+outlookNotPeopleWb = Workbook()
+outlookSortedWs = outlookSortedWb.active
+outlookFilteredWsPostfach = outlookFilteredWbPostfach.active
+outlookFilteredWsWeiterleitung = outlookFilteredWbWeiterleitung.active
+outlookNotPeopleWs = outlookNotPeopleWb.active
+
+exH.sort_outlook_ws(outlookSourceWs, outlookSortedWs)
+exH.filter_outlook_ws(
+    outlookSortedWs, outlookFilteredWsPostfach, outlookFilteredWsWeiterleitung, outlookNotPeopleWs)
+outlookSortedWb.save(exH.path + "\data\outlook_data_sorted.xlsx")
+outlookFilteredWbPostfach.save(
+    exH.path + "\data\outlook_data_filtered_postfach.xlsx")
+outlookFilteredWbWeiterleitung.save(
+    exH.path + "\data\outlook_data_filtered_weiterleitung.xlsx")
+outlookNotPeopleWb.save(exH.path + "\data\outlook_data_no_people.xlsx")
+outlookNotPeopleWb.save(exH.path + "\data\outlook_data_no_people.xlsx")
