1d04656822897c84ebef609df8b3e8a22ea68c02
qSenkMillo <nicolaspluci@gmx.de>
2022-03-17 22:36:05 +0100

    Anpassung der .env.sample datei und der setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,6 @@
 from setuptools import find_packages, setup
 
-setup(name="MV_Automatisierung", version="1.0", packages=find_packages())
+setup(name="MV_Automatisierung",
+      version="1.0",
+      packages=find_packages(),
+      install_requires=["openpyxl", "requests", "python-decouple"])
