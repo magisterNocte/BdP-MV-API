77fbbf5d5fc8ecd116c240e93317f5a3d827f3ac
magisterNocte <yannik.menge@gmail.com>
2022-03-21 14:58:33 +0100

    formatter as dependency option

--- src/setup.py
+++ src/setup.py
@@ -3,4 +3,8 @@ from setuptools import find_packages, setup
 setup(name="MV_Automatisierung",
       version="1.0",
       packages=find_packages(),
-      install_requires=["openpyxl", "requests", "python-decouple"])
+      install_requires=["openpyxl", "requests", "python-decouple"],
+      extras_require={
+          'opt1': ['autopep8'],
+      }
+      )
