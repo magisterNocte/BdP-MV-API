c49d88b63c9c56d69e912c34285b5c2745ffa855
magisterNocte <yannik.menge@gmail.com>
2022-03-21 14:32:15 +0100

    af

--- src/Apps/KJP Ausfüllen/KJPausfüllen.py
+++ src/Apps/KJP Ausfüllen/KJPausfüllen.py
@@ -11,52 +11,55 @@ username = config("USER")
 password = config("PASSWORD")
 
 
-
 config = []
 nami = Nami(config)
 nami.auth(username, password)
 
-#excel init
+# excel init
 sourceWb = load_workbook(path + "\data\data.xlsx")
-sourceWs= sourceWb.active
+sourceWs = sourceWb.active
+
 
 def fillInExcel(userDetails, thisRow):
 
-    try:    
-        if isinstance(userDetails[0],int) == True:
+    try:
+        if isinstance(userDetails[0], int) == True:
 
-            sourceWs["C"+ thisRow] = userDetails[1]["strasse"]
-            sourceWs["D"+  thisRow] = userDetails[1]["plz"] + " " + userDetails[1]["ort"]
-            sourceWs["E"+  thisRow] = "m" if userDetails[1]["geschlecht"] == "männlich" else "w"
-            sourceWs["F"+ thisRow] = UID.plzZuBundesland(userDetails[1]["plz"])
-            sourceWs["G"+ thisRow] = "E"
-            sourceWs["H"+ thisRow] = UID.userFunktion(nami, userDetails[0], UID.bulaTätigkeitenIDs)[0:-8]
-            sourceWs["I"+ thisRow] = UID.stammesIdToLV(userDetails[1]["gruppierung"])[1]
-            sourceWs["J"+ thisRow] = UID.getUserefZInfo(nami,userDetails[0])
+            sourceWs["C" + thisRow] = userDetails[1]["strasse"]
+            sourceWs["D" + thisRow] = userDetails[1]["plz"] + \
+                " " + userDetails[1]["ort"]
+            sourceWs["E" + thisRow] = "m" if userDetails[1]["geschlecht"] == "männlich" else "w"
+            sourceWs["F" +
+                     thisRow] = UID.plzZuBundesland(userDetails[1]["plz"])
+            sourceWs["G" + thisRow] = "E"
+            sourceWs["H" + thisRow] = UID.userFunktion(
+                nami, userDetails[0], UID.bulaTätigkeitenIDs)[0:-8]
+            sourceWs["I" +
+                     thisRow] = UID.stammesIdToLV(userDetails[1]["gruppierung"])[1]
+            sourceWs["J" + thisRow] = UID.getUserEfZInfo(nami, userDetails[0])
         else:
-            sourceWs["C"+ thisRow] = userDetails[0]
+            sourceWs["C" + thisRow] = userDetails[0]
     except:
-        sourceWs["C"+  thisRow] = "ERROR: Fehler beim Excel ausfüllen!"
-    
+        sourceWs["C" + thisRow] = "ERROR: Fehler beim Excel ausfüllen!"
+
 
 thisRow = 1
 for row in sourceWs.iter_rows():
 
     try:
-        vornameTemp = sourceWs["A"+ str(thisRow)].value.strip().split()[0]
-        nachnameTemp = sourceWs["B"+ str(thisRow)].value.strip()
-        print(vornameTemp,nachnameTemp)
+        vornameTemp = sourceWs["A" + str(thisRow)].value.strip().split()[0]
+        nachnameTemp = sourceWs["B" + str(thisRow)].value.strip()
+        print(vornameTemp, nachnameTemp)
         user = nami.user(vornameTemp, nachnameTemp)
-        userDetails = UID.getUserIDAndData(nami, user,vornameTemp, nachnameTemp)
- 
-        fillInExcel(userDetails,str(thisRow))
-        
+        userDetails = UID.getUserIDAndData(
+            nami, user, vornameTemp, nachnameTemp)
+
+        fillInExcel(userDetails, str(thisRow))
+
     except:
-        sourceWs["C"+  str(thisRow)] = "ERROR!"
+        sourceWs["C" + str(thisRow)] = "ERROR!"
 
-    thisRow+=1
-
+    thisRow += 1
 
 
 sourceWb.save(path + "\data\data.xlsx")
-
